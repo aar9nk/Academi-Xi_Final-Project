c1561dfcbbdb72f8516947211b043c8325d7a351
mitch <mitchellanderson202@hotmail.com>
2022-01-30 11:59:55 +1100

    changes made

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
+import { useState } from 'react';
 
 function App() {
+  const [contactOpen, setContactOpen] = useState(false);
   return (
     <div className="App">
-      <NavBar />
+      <NavBar contactOpen={contactOpen} setContactOpen={setContactOpen}/>
       <Main />
     </div>
   );
--- project/src/components/NavBar.js
+++ project/src/components/NavBar.js
@@ -1,41 +1,18 @@
 import "../../src/index.css";
 import { useState } from "react";
 import Contact from "./Contact";
-import More from "./More";
 import Button from "./Button";
 
-function NavBar() {
-    // const [showForm, setShowForm] = useState(false);
-
-    // const toggleClass = () => {
-    //     setShowForm(!showForm);
-    // }
-    
+function NavBar({ contactOpen }) {    
     return(
         <div className="navbar">
             <h2>&#x27A2; Mitch</h2>
             <h2>&#x260E; 0400 000 000</h2>
             <h2>&#x2709; mitch@fake.com</h2>
-            <More />
+            <Button onClick={contactOpen}/>
         </div>
     )
 }
 
 
 export default NavBar;
-
-
-// <Button onClick={toggleClass} />
-// <Form className={showForm ? "form-visible" : "form-hidden"} />
-
-
-
-
-// { showForm ? <Form className="form-hidden" /> : <More className="form-visible" /> }
-
-// {<More />
-
-// <h2 className="hamburger-button" onClick={setShowForm}>&#8801;</h2>
-// {
-//     showForm ? <Form className="off"/> : <Form className="on" />
-// } 
